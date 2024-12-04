e5360756b9afa2b36f28bcb9e72489169a95b37f
Trempadeira <123904293+Trempadeira@users.noreply.github.com>
2024-12-03 21:38:19 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -4,12 +4,12 @@ USE street_store;
 
 CREATE TABLE usuarios (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    city VARCHAR(255),
-    phone_number VARCHAR(20),
-    neighborhood VARCHAR(255)
+    name VARCHAR(25),
+    email VARCHAR(60),
+    password TEXT,
+    city VARCHAR(35),
+    phone_number VARCHAR(13),
+    neighborhood VARCHAR(20)
 );
 
 INSERT INTO usuarios (name, email, password, city, phone_number, neighborhood)
